f1bc23b1dc39d875e4e70c820627da80f44cdacc
maremaric <markoreus888@gmail.com>
2020-10-12 15:21:11 +0200

    vezba

---
+++ Javascript - zadatak4/script.js
@@ -0,0 +1,19 @@
+// Dat je niz stringova. Neki od tih stringova sadrze i cifre.
+
+// Kreirati novi niz celih brojeva, koji iz pocetnok niza uzima
+// samo one elemente koji sadrze cifre i izbacuje sve one znake koji nisu cifre.
+
+// Primer: Ulazni niz: ["abc","1ab22x","5","5ga6","ayz",""] bi
+// kao rezultat treablo da proizvede: [122,5,56]
+
+
+
+
+
+
+
+
+
+
+
+
