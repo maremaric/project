237cde86688ee260bb61c729ddfcc159fd581432
maremaric <markoreus888@gmail.com>
2020-10-14 17:01:50 +0200

    zadatak

--- Javascript - zadatak6/script.js
+++ Javascript - zadatak6/script.js
@@ -27,5 +27,35 @@
 // ]
 // zahteva tri ucionice.
 
+let vremena = [
+    { start: 10, kraj: 25},
+    { start: 20, kraj: 40},
+    { start: 35, kraj: 50}
+];
+
+let vremena1 = [
+    { start: 10, kraj: 15},
+    { start: 20, kraj: 40},
+    { start: 35, kraj: 50}
+];
+
+checkIntervals(vremena);
+
+function checkIntervals(niz){
+
+var brUcionica = 1;
+
+for(var i = 0; i < niz.length; i++){
+    for(var j = i + 1; j < niz.length; j++) {
+        if((niz[j].start <= niz[i].start && niz[j].kraj >= niz[i].start)
+            || (niz[j].start <= niz[i].kraj && niz[j].kraj >= niz[i].kraj)
+            || (niz[j].start >= niz[i].start && niz[j].kraj <= niz[i].kraj)
+        )
+        brUcionica++;
+    }
+}
+console.log("Broj učionica: " + brUcionica);
+}
+
 
 
