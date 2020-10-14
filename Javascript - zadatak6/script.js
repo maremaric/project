174b45650f98f022b9ec9af7d8a4197322c7677a
maremaric <markoreus888@gmail.com>
2020-10-14 16:55:52 +0200

    zadatak

---
+++ Javascript - zadatak6/script.js
@@ -0,0 +1,31 @@
+// Dat je niz pbjekata od kojih svaki ima format oblika: {start:10,kraj:15}. 
+// Svaki od ovih objekata reprezentuje vremena pocetka i kraja casa, 
+// izrazeno u minutima. Ako se dva intervala preklapaju, potrebne su dve ucionice. 
+
+// Odrediti koliko je minimalno ucionica potrebno da bi se sproveli svi casovi.
+
+// Primeri: let vremena = [
+//     {start: 10,kraj: 15},
+//     {start: 20,kraj: 30}
+// ]
+// zahteva jednu ucionicu. 
+// [
+//     {start: 10,kraj: 25},
+//     {start: 20,kraj: 30}
+// ]
+// zahteva dve ucionice .
+// [
+//     {start: 10,kraj: 20},
+//     {start: 30,kraj: 40},
+//     {start: 15,kraj: 25}
+// ]
+// zahteva dve ucionice.
+// [
+//     {start: 10,kraj: 25},
+//     {start: 20,kraj: 40},
+//     {start: 35,kraj: 50}
+// ]
+// zahteva tri ucionice.
+
+
+
