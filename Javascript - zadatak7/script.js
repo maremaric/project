8627b933025aa591830cf619b5a4ce5b0745e80a
maremaric <markoreus888@gmail.com>
2020-10-15 14:42:33 +0200

    zadatak

---
+++ Javascript - zadatak7/script.js
@@ -0,0 +1,13 @@
+// Dat je string koji sadrzi samo slova engleske abecede i to samo velika slova. 
+
+// Implementirati enkodiranje i dekodiranje tog stringa tako da se pri enkodiranju svako 
+// visestruko pojavljivanje slova enkodira kao broj koji predstavlja koliko se 
+// puta slovo pojavljuje, a potom samo slovo. Ako se slovo pojavljuje 
+// samo jednom, onda se u enkodiranom stringu to pojavljuje kao samo 
+// jedno pojavljivanje tog slova, bez navodjenja broja 1. 
+
+// Primer: Ako je string na primer: "AAAABBBXYCCDAA", 
+// enkodirani oblik bi bio: 4A3BXY2CD2A
+
+
+
