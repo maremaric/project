ed53a568fb4facf5cb147bcf5859a6597b1a830e
maremaric <markoreus888@gmail.com>
2020-10-15 14:46:22 +0200

    zadatak

--- Javascript - zadatak7/script.js
+++ Javascript - zadatak7/script.js
@@ -10,4 +10,52 @@
 // enkodirani oblik bi bio: 4A3BXY2CD2A
 
 
+var string = "AAAABBBXYCCDAA";
+
+string = encode(string);
+console.log(string);
+string = decode(string);
+console.log(string);
+
+function encode(string){
+var result = "";
+var char = string[0];
+var num = 1;
+for(var i = 1; i < string.length; i++){
+    if(char === string[i]){
+        num++;			
+    }else{
+        if(num == 1)
+            result += char;
+        else
+            result += num+char;
+        char = string[i];
+        num = 1;
+    }
+}
+
+if(num == 1)
+    result += char;
+else
+    result += num+char;
+return result;
+}
+
+function decode(string){
+var result = "";
+var num = 1;
+var char = "";
+
+for(var i = 0; i < string.length; i++){
+    char = string[i];
+    if(char.match(/[1-9]/g) != null){
+        num = char;	 		
+    }else{
+        for(var j = 0; j < num; j++)
+            result += char;
+        num = 1;
+    }
+}
+return result;
+}
 
