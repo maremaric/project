bf60447f08994acd1262f78d470e8dfd38d785e5
maremaric <markoreus888@gmail.com>
2020-10-26 13:59:42 +0100

    vezba

---
+++ Javascript - primeri/factorialize.js
@@ -0,0 +1,12 @@
+function factorialize(num) {
+
+    let result = 1;
+
+    for(let i = 1; i <= num; i++) {
+        // result = result * i;
+        result *= i;
+    }
+    return result;
+}
+
+console.log(factorialize(5));
