8c06303e7dc7ba14818703b42f8a23cea788a994
maremaric <markoreus888@gmail.com>
2020-10-30 18:05:50 +0100

    vezba

--- Javascript - primeri/findLongestWord.js
+++ Javascript - primeri/findLongestWord.js
@@ -17,3 +17,12 @@ console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 console.log('------------------------------------------------------');
 
 
+// Primer 2:
+
+function findLoWo(str) {
+    return str.split(" ").sort(function(a, b) {return b.length - a.length})[0];
+}
+
+console.log(findLoWo("The quick brown fox jumped over the lazy dog"));
+
+
